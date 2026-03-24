7d3e07db71b037dc264fe838ab7fa0a706db8060
Rogério Lino <rogeriolino@gmail.com>
2026-03-24 10:35:14 -0300

    chore: symfony 7.4 deprecations (#8)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -29,7 +29,7 @@ use Novosga\UsersBundle\NovosgaUsersBundle;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\View\TwitterBootstrap5View;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
